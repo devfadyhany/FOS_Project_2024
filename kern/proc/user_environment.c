a08976befc95c93eeb516c9fd007762225680023
Fady Hany <121233048+devfadyhany@users.noreply.github.com>
2024-11-21 23:33:02 +0200

    Update create user kern stack

--- kern/proc/user_environment.c
+++ kern/proc/user_environment.c
@@ -868,7 +868,7 @@ void* create_user_kern_stack(uint32* ptr_user_page_directory)
 	//TODO: [PROJECT'24.MS2 - #07] [2] FAULT HANDLER I - create_user_kern_stack
 	// Write your code here, remove the panic and write your code
 	//panic("create_user_kern_stack() is not implemented yet...!!");
-	uint32* ptr_stack = (uint32*)kmalloc(KERNEL_STACK_SIZE);
+	uint32* ptr_stack = (KERNEL_STACK_SIZE);
 
 	if (ptr_stack == NULL){
 		panic("Failed to create user kernel stack!");
