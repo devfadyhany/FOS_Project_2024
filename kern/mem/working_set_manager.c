f05e2c1f49df4361174a6b1ba8d8705eea3d2d69
Fady Hany <121233048+devfadyhany@users.noreply.github.com>
2024-11-21 11:05:13 +0200

    Implement env_page_ws_list_create_element

--- kern/mem/working_set_manager.c
+++ kern/mem/working_set_manager.c
@@ -19,9 +19,17 @@ inline struct WorkingSetElement* env_page_ws_list_create_element(struct Env* e, 
 	//TODO: [PROJECT'24.MS2 - #07] [2] FAULT HANDLER I - Create a new WS element
 	//If failed to create a new one, kernel should panic()!
 	//COMMENT THE FOLLOWING LINE BEFORE START CODING
-	panic("env_page_ws_list_create_element is not implemented yet");
+	//panic("env_page_ws_list_create_element is not implemented yet");
 	//Your Code is Here...
+	struct WorkingSetElement* wsElement = (struct WorkingSetElement*)kmalloc(sizeof(struct WorkingSetElement));
 
+	if (wsElement == NULL){
+		panic("Failed to allocate working_set_element");
+	}
+
+	wsElement->virtual_address = virtual_address;
+
+	return wsElement;
 }
 inline void env_page_ws_invalidate(struct Env* e, uint32 virtual_address)
 {
